c4285f891731493f035ad1a95785f571d3c7b7f9
leohuang990 <69603987+leohuang990@users.noreply.github.com>
2021-01-11 16:12:32 -0500

    Update key.js

--- server/config/key.js
+++ server/config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://hhp990930:990930hhp@cluster0.yg1kl.mongodb.net/chat-app?retryWrites=true&w=majority',
-    secret: "hhp"
+    mongoURI: 'your link',
+    secret: "your secret"
 }
